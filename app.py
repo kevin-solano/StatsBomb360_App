00e97c1592bdafe22df21e03bb7868c9f3cf5135
Aleks Kapich <aleks.kapich@gmail.com>
2024-12-24 00:18:59 +0100

    Update outdated parameter

--- app.py
+++ app.py
@@ -1,78 +1,140 @@
 import streamlit as st
 from streamlit_extras.badges import badge
-from auxiliary import match_dict, matches, create_timestring, get_event_dict, create_plot, shot_freeze_frame
+from auxiliary import (
+    match_dict,
+    matches,
+    create_timestring,
+    get_event_dict,
+    create_plot,
+    shot_freeze_frame,
+)
 from statsbombpy import sb
 import pandas as pd
 
 
 st.set_page_config(
-        page_title="Euro 2024: StatsBomb 360° Data Explorer",
-        page_icon='https://raw.githubusercontent.com/AKapich/StatsBomb360_App/main/logos/eurologo.ico',
-    )
+    page_title="Euro 2024: StatsBomb 360° Data Explorer",
+    page_icon="https://raw.githubusercontent.com/AKapich/StatsBomb360_App/main/logos/eurologo.ico",
+)
 
 
 st.title("Euro 2024: StatsBomb 360° Tool")
-st.markdown("*Platform providing insight into StatsBomb 360° data for every Euro 2024 match*")
+st.markdown(
+    "*Platform providing insight into StatsBomb 360° data for every Euro 2024 match*"
+)
 st.markdown("---")
 
-st.sidebar.image("https://raw.githubusercontent.com/AKapich/StatsBomb360_App/main/logos/EURO2024.png")
+st.sidebar.image(
+    "https://raw.githubusercontent.com/AKapich/StatsBomb360_App/main/logos/EURO2024.png"
+)
 
 # dropdown for choosing the match
 st.sidebar.title("Select Match")
 selected_match = st.sidebar.selectbox("Match:", match_dict.keys(), index=1)
 
-mode = st.sidebar.selectbox("Choose mode:", ['Timestamp Slider', 'Shot Freeze Frame'])
+mode = st.sidebar.selectbox("Choose mode:", ["Timestamp Slider", "Shot Freeze Frame"])
 
 match_id = match_dict[selected_match]
-home_team = selected_match.split(' - ')[0]
-away_team = selected_match.split(' - ')[1]
-competition_stage = matches[matches['match_id']==match_id].iloc[0]['competition_stage']
+home_team = selected_match.split(" - ")[0]
+away_team = selected_match.split(" - ")[1]
+competition_stage = matches[matches["match_id"] == match_id].iloc[0][
+    "competition_stage"
+]
 
 
 # data
-frame_df = sb.frames(match_id=match_id, fmt='dataframe')
-frame_df = frame_df.rename(columns={'location': 'player_location'})
+frame_df = sb.frames(match_id=match_id, fmt="dataframe")
+frame_df = frame_df.rename(columns={"location": "player_location"})
 event_df = sb.events(match_id=match_id)
-event_df['timestring'] = event_df.apply(create_timestring, axis=1)
+event_df["timestring"] = event_df.apply(create_timestring, axis=1)
 
-df = pd.merge(frame_df, event_df, on='id', how='right')
-df = df.sort_values('timestring')
+df = pd.merge(frame_df, event_df, on="id", how="right")
+df = df.sort_values("timestring")
 
-if mode == 'Timestamp Slider':
-    chosen_timestamp = st.select_slider("Select timestamp", options=df[(df['visible_area'].notna()) | (df['type']=='Shot')]['timestring'].unique())
+if mode == "Timestamp Slider":
+    chosen_timestamp = st.select_slider(
+        "Select timestamp",
+        options=df[(df["visible_area"].notna()) | (df["type"] == "Shot")][
+            "timestring"
+        ].unique(),
+    )
     event_dict = get_event_dict(df=df, chosen_timestamp=chosen_timestamp)
 
     displayed_event = st.selectbox("Select event", options=event_dict.keys())
     voronoi = st.checkbox("Highlight controlled space")
 
-    st.pyplot(create_plot(df, event_dict, chosen_timestamp, displayed_event, voronoi, home_team, away_team))
+    st.pyplot(
+        create_plot(
+            df,
+            event_dict,
+            chosen_timestamp,
+            displayed_event,
+            voronoi,
+            home_team,
+            away_team,
+        )
+    )
 
-elif mode == 'Shot Freeze Frame':
+elif mode == "Shot Freeze Frame":
     tab1, tab2 = st.tabs(["📈 Charts", "📄 Shot Info"])
-    shot_info = event_df[event_df['shot_outcome'].notna()][['player', 'timestring', 'shot_outcome', 'shot_statsbomb_xg', 'shot_technique', 'shot_body_part']]
-    shot_info.index = range(1, len(shot_info)+1)
+    shot_info = event_df[event_df["shot_outcome"].notna()][
+        [
+            "player",
+            "timestring",
+            "shot_outcome",
+            "shot_statsbomb_xg",
+            "shot_technique",
+            "shot_body_part",
+        ]
+    ]
+    shot_info.index = range(1, len(shot_info) + 1)
 
     with tab1:
-        shot_cols = ['player', 'team', 'timestring', 'shot_outcome', 'shot_freeze_frame', 'location', 'shot_end_location']
-        shot_df = event_df[event_df['shot_outcome'].notna()][shot_cols]
-        shot_df['tag'] = shot_df['player'] + ' - ' + shot_df['timestring'] + ' ( ' + shot_df['shot_outcome'] + ' )'
+        shot_cols = [
+            "player",
+            "team",
+            "timestring",
+            "shot_outcome",
+            "shot_freeze_frame",
+            "location",
+            "shot_end_location",
+        ]
+        shot_df = event_df[event_df["shot_outcome"].notna()][shot_cols]
+        shot_df["tag"] = (
+            shot_df["player"]
+            + " - "
+            + shot_df["timestring"]
+            + " ( "
+            + shot_df["shot_outcome"]
+            + " )"
+        )
 
-        tag = st.selectbox("Choose shot",options=shot_df['tag'].to_list())
+        tag = st.selectbox("Choose shot", options=shot_df["tag"].to_list())
         keeper_cone = st.checkbox("Highlight Keeper Cone View")
 
         st.pyplot(shot_freeze_frame(shot_df, tag, home_team, away_team, keeper_cone))
 
     with tab2:
-        shot_info.columns = ["Player", "Timestamp", "Shot Outcome", "xG", "Technique", "Body Part"]
+        shot_info.columns = [
+            "Player",
+            "Timestamp",
+            "Shot Outcome",
+            "xG",
+            "Technique",
+            "Body Part",
+        ]
         st.dataframe(shot_info)
 
 
-st.markdown('---')
-st.image('https://raw.githubusercontent.com/AKapich/StatsBomb360_App/main/logos/sb_icon.png',
-          caption='App made by Aleks Kapich. Data powered by StatsBomb', use_column_width=True)
+st.markdown("---")
+st.image(
+    "https://raw.githubusercontent.com/AKapich/StatsBomb360_App/main/logos/sb_icon.png",
+    caption="App made by Aleks Kapich. Data powered by StatsBomb",
+    use_container_width=True,
+)
 
 # signature
-st.sidebar.markdown('---')
+st.sidebar.markdown("---")
 col1, col2 = st.columns(2)
 with col1:
     st.sidebar.markdown("App made by **Aleks Kapich**")
--- auxiliary.py
+++ auxiliary.py
@@ -6,9 +6,12 @@ from mplsoccer import Pitch
 
 # Data from Euro 2024
 matches = sb.matches(competition_id=55, season_id=282)
-match_dict = {home+' - '+away: match_id
-                 for match_id, home, away
-                 in zip(matches['match_id'], matches['home_team'], matches['away_team'])}
+match_dict = {
+    home + " - " + away: match_id
+    for match_id, home, away in zip(
+        matches["match_id"], matches["home_team"], matches["away_team"]
+    )
+}
 
 
 country_colors = {
@@ -24,200 +27,237 @@ country_colors = {
     "England": "#002366",
     "Serbia": "#DC143C",
     "Switzerland": "#FF0000",
-    "Scotland": '#006cb7',
-    'Hungary': '#008d55',
-    'Albania': '#ed1b24',
-    'Italy': '#009247',
-    'Slovenia': '#005aab',
-    'Austria': '#ed1b24',
-    'Slovakia': '#005aab',
-    'Romania': '#ffde00',
-    'Ukraine': '#005aab',
-    'Turkey': '#ed1b24',
-    'Georgia': '#fffffc',
-    'Czech Republic': '#005aab'
+    "Scotland": "#006cb7",
+    "Hungary": "#008d55",
+    "Albania": "#ed1b24",
+    "Italy": "#009247",
+    "Slovenia": "#005aab",
+    "Austria": "#ed1b24",
+    "Slovakia": "#005aab",
+    "Romania": "#ffde00",
+    "Ukraine": "#005aab",
+    "Turkey": "#ed1b24",
+    "Georgia": "#fffffc",
+    "Czech Republic": "#005aab",
 }
 
 
 def create_timestring(row):
-    second_component = str(row['second']) if row['second'] > 9 else '0'+str(row['second'])
-    minute_component = str(row['minute']) if row['minute'] > 9 else '0'+str(row['minute'])
-    return minute_component + ':' + second_component
+    second_component = (
+        str(row["second"]) if row["second"] > 9 else "0" + str(row["second"])
+    )
+    minute_component = (
+        str(row["minute"]) if row["minute"] > 9 else "0" + str(row["minute"])
+    )
+    return minute_component + ":" + second_component
 
 
 def get_event_dict(df, chosen_timestamp):
-    timestamp_events = df[df['timestring']==chosen_timestamp]
-    timestamp_events = timestamp_events[timestamp_events['visible_area'].notna()]
+    timestamp_events = df[df["timestring"] == chosen_timestamp]
+    timestamp_events = timestamp_events[timestamp_events["visible_area"].notna()]
 
-    timestamp_events['event_tag'] = timestamp_events['player'] + ': ' + timestamp_events['type']
-    event_dict = dict(zip(timestamp_events['event_tag'], timestamp_events['id']))
+    timestamp_events["event_tag"] = (
+        timestamp_events["player"] + ": " + timestamp_events["type"]
+    )
+    event_dict = dict(zip(timestamp_events["event_tag"], timestamp_events["id"]))
     event_dict = {k: v for k, v in event_dict.items() if not pd.isna(k)}
     return event_dict
 
 
-def create_plot(df, event_dict, chosen_timestamp, displayed_event, voronoi, home_team, away_team):
+def create_plot(
+    df, event_dict, chosen_timestamp, displayed_event, voronoi, home_team, away_team
+):
     home_color = country_colors[home_team]
     away_color = country_colors[away_team]
 
-    frame = df[df['timestring']==chosen_timestamp]
-    frame = frame[frame['id']==event_dict[displayed_event]]
+    frame = df[df["timestring"] == chosen_timestamp]
+    frame = frame[frame["id"] == event_dict[displayed_event]]
 
-    fig ,ax = plt.subplots(figsize=(13, 8),constrained_layout=False, tight_layout=True)
-    fig.set_facecolor('#0e1117')
-    ax.patch.set_facecolor('#0e1117')
-    pitch = Pitch(pitch_type='statsbomb', pitch_color='#0e1117', line_color='#c7d5cc')
+    fig, ax = plt.subplots(figsize=(13, 8), constrained_layout=False, tight_layout=True)
+    fig.set_facecolor("#0e1117")
+    ax.patch.set_facecolor("#0e1117")
+    pitch = Pitch(pitch_type="statsbomb", pitch_color="#0e1117", line_color="#c7d5cc")
     pitch.draw(ax=ax)
 
     visible_area = np.array(frame.iloc[0].visible_area).reshape(-1, 2)
-    polygon_color = 'None' if voronoi else 'gray'
-    visible = pitch.polygon([visible_area], color=polygon_color, linestyle='-.', lw=3, ax=ax, ec='white', alpha=0.2)
+    polygon_color = "None" if voronoi else "gray"
+    visible = pitch.polygon(
+        [visible_area],
+        color=polygon_color,
+        linestyle="-.",
+        lw=3,
+        ax=ax,
+        ec="white",
+        alpha=0.2,
+    )
 
     for _, row in frame.iterrows():
-        if row['team'] == home_team:
-            color = home_color if row['teammate'] else away_color
-        elif row['team'] == away_team:
-            color = away_color if row['teammate'] else home_color
-        marker = 'o' if not row['keeper'] else 'D'
+        if row["team"] == home_team:
+            color = home_color if row["teammate"] else away_color
+        elif row["team"] == away_team:
+            color = away_color if row["teammate"] else home_color
+        marker = "o" if not row["keeper"] else "D"
         size = 70
-        if row['actor']:
-            marker = '*'
+        if row["actor"]:
+            marker = "*"
             size = 200
         plt.scatter(
-            x=row['player_location'][0],
-            y=row['player_location'][1],
+            x=row["player_location"][0],
+            y=row["player_location"][1],
             color=color,
             marker=marker,
             s=size,
-            edgecolors='black'
+            edgecolors="black",
         )
 
-    row = df[df['id']==event_dict[displayed_event]].iloc[0]
+    row = df[df["id"] == event_dict[displayed_event]].iloc[0]
     pitch.annotate(
-        displayed_event.split(': ')[0],
-        xy=(row["location"][0], row["location"][1]-2),
-        c='white', va='center', ha='center',
-        size=9, fontweight='bold', ax=ax
+        displayed_event.split(": ")[0],
+        xy=(row["location"][0], row["location"][1] - 2),
+        c="white",
+        va="center",
+        ha="center",
+        size=9,
+        fontweight="bold",
+        ax=ax,
     )
 
     if voronoi:
-        team1, team2 = pitch.voronoi([loc[0] for loc in frame['player_location']], [loc[1] for loc in frame['player_location']], frame['teammate'])
-        if row['team'] == away_team:
+        team1, team2 = pitch.voronoi(
+            [loc[0] for loc in frame["player_location"]],
+            [loc[1] for loc in frame["player_location"]],
+            frame["teammate"],
+        )
+        if row["team"] == away_team:
             team1, team2 = team2, team1
-        t1 = pitch.polygon(team1, ax=ax, fc=home_color, ec='gray', lw=3, alpha=0.3)
-        t2 = pitch.polygon(team2, ax=ax, fc=away_color, ec='gray', lw=3, alpha=0.3)
+        t1 = pitch.polygon(team1, ax=ax, fc=home_color, ec="gray", lw=3, alpha=0.3)
+        t2 = pitch.polygon(team2, ax=ax, fc=away_color, ec="gray", lw=3, alpha=0.3)
         for p1 in t1:
             p1.set_clip_path(visible[0])
         for p2 in t2:
             p2.set_clip_path(visible[0])
 
-    if row['type'] == 'Carry':
+    if row["type"] == "Carry":
         pitch.arrows(
             xstart=row["location"][0],
             ystart=row["location"][1],
-            xend=row['carry_end_location'][0],
-            yend=row['carry_end_location'][1],
+            xend=row["carry_end_location"][0],
+            yend=row["carry_end_location"][1],
             ax=ax,
-            color='white',
-            linestyle='--'
+            color="white",
+            linestyle="--",
         )
-    elif row['type'] in ['Pass', 'Shot']:
-        type = 'pass' if row['type'] == 'Pass' else 'shot'
+    elif row["type"] in ["Pass", "Shot"]:
+        type = "pass" if row["type"] == "Pass" else "shot"
         pitch.lines(
             xstart=row["location"][0],
             ystart=row["location"][1],
-            xend=row[f'{type}_end_location'][0],
-            yend=row[f'{type}_end_location'][1],
+            xend=row[f"{type}_end_location"][0],
+            yend=row[f"{type}_end_location"][1],
             ax=ax,
             comet=True,
-            color='white'
+            color="white",
         )
 
     return fig
 
 
 def shot_freeze_frame(shot_df, tag, home_team, away_team, keeper_cone=False):
-    shot = shot_df[shot_df['tag']==tag].iloc[0]
+    shot = shot_df[shot_df["tag"] == tag].iloc[0]
     home_color = country_colors[home_team]
     away_color = country_colors[away_team]
 
-    fig ,ax = plt.subplots(figsize=(13, 8),constrained_layout=False, tight_layout=True)
-    fig.set_facecolor('#0e1117')
-    ax.patch.set_facecolor('#0e1117')
-    pitch = Pitch(pitch_type='statsbomb', pitch_color='#0e1117', line_color='#c7d5cc')
+    fig, ax = plt.subplots(figsize=(13, 8), constrained_layout=False, tight_layout=True)
+    fig.set_facecolor("#0e1117")
+    ax.patch.set_facecolor("#0e1117")
+    pitch = Pitch(pitch_type="statsbomb", pitch_color="#0e1117", line_color="#c7d5cc")
     pitch.draw(ax=ax)
 
-    for player in shot['shot_freeze_frame']:
-        if shot['team'] == home_team:
-            color = home_color if player['teammate'] else away_color
-        elif shot['team'] == away_team:
-            color = away_color if player['teammate'] else home_color
-        
+    for player in shot["shot_freeze_frame"]:
+        if shot["team"] == home_team:
+            color = home_color if player["teammate"] else away_color
+        elif shot["team"] == away_team:
+            color = away_color if player["teammate"] else home_color
+
         plt.scatter(
-            x=player['location'][0],
-            y=player['location'][1],
+            x=player["location"][0],
+            y=player["location"][1],
             color=color,
-            marker='o',
+            marker="o",
             s=100,
-            edgecolors='black'
+            edgecolors="black",
         )
 
     if keeper_cone:
-        pitch.goal_angle(shot['location'][0], shot['location'][1], alpha=0.47, color='cyan', ax=ax)
-        for player in shot['shot_freeze_frame']:
-            if player['position']['name'] == 'Goalkeeper':
-                plt.scatter(player['location'][0], player['location'][1], color='white', marker='p', s=220, edgecolors='black')
+        pitch.goal_angle(
+            shot["location"][0], shot["location"][1], alpha=0.47, color="cyan", ax=ax
+        )
+        for player in shot["shot_freeze_frame"]:
+            if player["position"]["name"] == "Goalkeeper":
+                plt.scatter(
+                    player["location"][0],
+                    player["location"][1],
+                    color="white",
+                    marker="p",
+                    s=220,
+                    edgecolors="black",
+                )
                 ax.annotate(
-                    'G',
-                    xy=(player['location'][0], player['location'][1]+0.1),
-                    c='black',
-                    va='center',
-                    ha='center',
+                    "G",
+                    xy=(player["location"][0], player["location"][1] + 0.1),
+                    c="black",
+                    va="center",
+                    ha="center",
                     size=9,
-                    fontweight='bold',
+                    fontweight="bold",
                 )
                 break
 
     pitch.annotate(
-        shot['player'],
-        xy=(shot['location'][0], shot['location'][1]-2),
-        c='white', va='center', ha='center',
-        size=9, fontweight='bold', ax=ax
+        shot["player"],
+        xy=(shot["location"][0], shot["location"][1] - 2),
+        c="white",
+        va="center",
+        ha="center",
+        size=9,
+        fontweight="bold",
+        ax=ax,
     )
-    color = home_color if shot['team'] == home_team else away_color
+    color = home_color if shot["team"] == home_team else away_color
     plt.scatter(
-        x=shot['location'][0],
-        y=shot['location'][1],
+        x=shot["location"][0],
+        y=shot["location"][1],
         color=color,
-        marker='*',
+        marker="*",
         s=450,
-        edgecolors='black'
+        edgecolors="black",
     )
     pitch.lines(
-        xstart=shot['location'][0],
+        xstart=shot["location"][0],
         ystart=shot["location"][1],
-        xend=shot['shot_end_location'][0],
-        yend=shot['shot_end_location'][1],
+        xend=shot["shot_end_location"][0],
+        yend=shot["shot_end_location"][1],
         ax=ax,
         comet=True,
-        color='white'
+        color="white",
     )
 
-    if shot['shot_outcome'] in ['Blocked', 'Saved']:
-        color = away_color if shot['team'] == home_team else home_color
-        marker='X'
-        size=300
-    elif shot['shot_outcome'] == 'Goal':
-        color='white'
-        marker='D'
-        size=120
-    if shot['shot_outcome'] in ['Blocked', 'Saved', 'Goal']:
+    if shot["shot_outcome"] in ["Blocked", "Saved"]:
+        color = away_color if shot["team"] == home_team else home_color
+        marker = "X"
+        size = 300
+    elif shot["shot_outcome"] == "Goal":
+        color = "white"
+        marker = "D"
+        size = 120
+    if shot["shot_outcome"] in ["Blocked", "Saved", "Goal"]:
         plt.scatter(
-            x=shot['shot_end_location'][0],
-            y=shot['shot_end_location'][1],
+            x=shot["shot_end_location"][0],
+            y=shot["shot_end_location"][1],
             color=color,
             marker=marker,
             s=size,
-            edgecolors='black'
+            edgecolors="black",
         )
-        
+
     return fig
